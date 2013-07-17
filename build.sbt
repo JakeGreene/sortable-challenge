3c6a9aae7b145645ecd3c6e0298b82b61bf45eeb
Jake Greene <jakeandrew.greene@gmail.com>
2013-07-16 20:50:29 -0400

    Niave Implementation
    Look for the name of the product in the title
    of the listings. No fancy rules or stats

--- build.sbt
+++ build.sbt
@@ -6,6 +6,7 @@ scalaVersion := "2.10.1"
 
 resolvers ++= Seq(
     "Typesafe Repository" at "http://repo.typesafe.com/typesafe/releases/",
+    "Sonatype OSS Snapshots" at "https://oss.sonatype.org/content/repositories/snapshots",
     "spray repo" at "http://repo.spray.io"
 )
 
@@ -13,5 +14,6 @@ scalacOptions := Seq("-unchecked", "-deprecation", "-encoding", "utf8")
 
 libraryDependencies ++= Seq( 
     "com.typesafe.akka" %% "akka-actor" % "2.1.4",
+    "com.github.nscala-time" %% "nscala-time" % "0.4.2",
     "io.spray" %%  "spray-json" % "1.2.5"
 )
--- src/main/scala/ca/jakegreene/sortable/SortableChallenge.scala
+++ src/main/scala/ca/jakegreene/sortable/SortableChallenge.scala
@@ -1,5 +1,70 @@
 package ca.jakegreene.sortable
 
+import scala.io._
+import com.github.nscala_time.time.Imports._
+import spray.json._
+import java.io.File
+import java.io.FileWriter
+
+case class Product(product_name: String, manufacturer: String, family: Option[String], model: String, announced_date: DateTime)
+case class Listing(title: String, manufacturer: String, currency: String, price: String)
+case class Result(product_name: String, listings: Seq[Listing])
+
+object MyJsonProtocol extends DefaultJsonProtocol {
+  
+  implicit object DateTimeFormat extends RootJsonFormat[DateTime] {
+    def write(date: DateTime) = JsString(date.toString())
+    def read(value: JsValue) = value match {
+      case JsString(date) => new DateTime(date)
+      case _ => throw new DeserializationException("DateTime expected")
+    }
+  }
+  
+  implicit val productFormat = jsonFormat5(Product)
+  implicit val listingFormat = jsonFormat4(Listing)
+  implicit val resultFormat = jsonFormat2(Result)
+}
+
 object SortableChallenge extends App {
-	println("Hello World")
+  
+	import MyJsonProtocol._
+	
+	// Some of the product names and listing titles are in different languages
+	implicit val codec = Codec.UTF8
+	
+	val prodBuilder = Seq.newBuilder[Product]
+	for (line <- Source.fromFile("data/products.txt").getLines) {
+	  /*
+	   * Scala case classes cannot have dashes "-" in their names
+	   * but spray-json requires the case class member names to be the
+	   * same as the names in the JSON object.
+	   */ 
+	  val properLine = line.replaceAll("announced-date", "announced_date")
+	  val jsonAst = properLine.asJson
+	  prodBuilder += jsonAst.convertTo[Product]
+	}
+	val products = prodBuilder.result
+
+	val listBuilder = Seq.newBuilder[Listing]
+	for (line <- Source.fromFile("data/listings.txt").getLines) {
+	  val jsonAst = line.asJson
+	  listBuilder += jsonAst.convertTo[Listing]
+	}
+	val listings = listBuilder.result
+	
+	val resultBuilder = Seq.newBuilder[Result]
+	for (product <- products) {
+	  val matchBuilder = Seq.newBuilder[Listing]
+	  for (listing <- listings) {
+	    if (listing.title.contains(product.product_name.replace("_", " "))) {
+	      matchBuilder += listing
+	    }
+	  }
+	  resultBuilder += Result(product.product_name, matchBuilder.result)
+	}
+	val results = resultBuilder.result
+	
+	val writer = new FileWriter(new File("results.txt"))
+	results.map(r => r.toJson).foreach(json => writer.write(json.prettyPrint + '\n'))
+	writer.close()
 }
